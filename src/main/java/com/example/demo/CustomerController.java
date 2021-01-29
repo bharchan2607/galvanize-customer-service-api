e7b984d61538651e4d1ffc49a888c65a87c37125
Matt Yamarino <779649@cognizant.com>
2021-01-29 15:59:06 -0600

    added challenge methods to controller to return custom response

--- src/main/java/com/example/demo/CustomerController.java
+++ src/main/java/com/example/demo/CustomerController.java
@@ -19,10 +19,21 @@ public class CustomerController {
         return customerService.getAllCustomers();
     }
 
+    @GetMapping("/challenge/customers")
+    public CustomerResponse getAllCustomersResponse() {
+        return customerService.getAllCustomersResponse();
+    }
+
     @PostMapping("/customers")
     @ResponseStatus(HttpStatus.CREATED)
     public void addCustomer(@RequestBody Customer newCustomer) {
         customerService.addCustomer(newCustomer);
+    }
+
+    @PostMapping("/challenge/customers")
+    @ResponseStatus(HttpStatus.CREATED)
+    public CustomerResponse addCustomerResponse(@RequestBody Customer newCustomer) {
+        return customerService.addCustomerResponse(newCustomer);
     }
 
     @GetMapping("/customers/{customerId}")
@@ -30,14 +41,30 @@ public class CustomerController {
         return customerService.getCustomerById(customerId);
     }
 
+    @GetMapping("/challenge/customers/{customerId}")
+    public CustomerResponse getCustomerByIdResponse(@PathVariable String customerId) {
+        return customerService.getCustomerByIdResponse(customerId);
+    }
+
     @PutMapping("/customers/{customerId}")
     public Customer updateCustomer(@PathVariable String customerId, @RequestBody Customer customer) {
         return customerService.updateCustomer(customer);
+    }
+
+    @PutMapping("/challenge/customers/{customerId}")
+    public CustomerResponse updateCustomerResponse(@PathVariable String customerId, @RequestBody Customer customer) {
+        return customerService.updateCustomerResponse(customer);
     }
 
     @DeleteMapping("/customers/{customerId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeCustomer(@PathVariable String customerId) {
         customerService.removeCustomer(customerId);
+    }
+
+    @DeleteMapping("/challenge/customers/{customerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public CustomerResponse removeCustomerResponse(@PathVariable String customerId) {
+        return customerService.removeCustomerResponse(customerId);
     }
 }
